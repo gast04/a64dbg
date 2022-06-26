008b4d71a61203a2eccd85af6f12877645e1be38
niku <kurt.nistelberger@hotmail.com>
2022-06-26 20:06:45 +0200

    .

---
+++ tests/CppBinaries/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+	string filename = "/data/local/tmp/testfile";
+
+	fstream my_file;
+	my_file.open(filename, ios::in);
+	if (!my_file) {
+		cout << "Could not open file!\n";
+	}
+
+	string content;
+
+	my_file >> content;
+
+	cout << "Content: " << content << endl;
+
+
+	my_file.close(); 
+	return 0;
+}
